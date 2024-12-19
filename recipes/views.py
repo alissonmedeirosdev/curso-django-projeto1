ff64d57257aac479278b4ad38619e2080f604186
alissonmedeirosdev <alissonmelo.work@gmail.com>
2024-12-19 01:39:01 -0300

    refactor the code

--- recipes/views.py
+++ recipes/views.py
@@ -18,3 +18,6 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Alisson Medeiros',
     })
+    
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
