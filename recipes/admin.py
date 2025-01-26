0bf2948a9f72a44dbd5d2d7402b837c935c103da
alissonmedeirosdev <alissonmelo.work@gmail.com>
2025-01-26 09:41:09 -0300

    add database

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+
+"""
+    Para registar  a model na Admin
+"""
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+    
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
