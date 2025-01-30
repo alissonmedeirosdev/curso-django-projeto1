69b84e3d0be216efb1e965dc79fda974bee9ba71
alissonmedeirosdev <alissonmelo.work@gmail.com>
2025-01-29 23:32:04 -0300

    use o get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 # Create your views here.
@@ -24,10 +24,13 @@ def home(request):
     })
     
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True  
-    ).order_by('-id')
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id,
+    #     is_published=True  
+    # ).order_by('-id')
+    
+    # if not recipes:
+    #     raise Http404('Not Found 😓')
    
     # category_name = getattr(
     #     getattr(recipes.first(),'category', None),
@@ -35,12 +38,16 @@ def category(request, category_id):
     #     'Not Found 404'
     # )
     
-    if not recipes:
-        raise Http404('Not Found 😓')
+    # recipes = get_list_or_404(Recipe, category__id=category_id, is_published=True)
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True  
+    ).order_by('-id'), category__id=category_id, is_published=True)
     
+    print(recipes)
     return render(request,'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category',
+        'title': f'{recipes[0].category.name} - Category',
     })
     
 def recipe(request, id):
