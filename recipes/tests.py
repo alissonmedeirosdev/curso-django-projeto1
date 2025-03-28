3263b45adc721249c2b8ef7a875c4ddadf0332f4
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-03-27 21:53:33 -0300

    add new tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -6,5 +6,13 @@ class RecipeURLsTest(TestCase):
         assert 1 == 1
 
     def test_recipe_home_url_is_corret(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_corret(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_detail_url_is_corret(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
