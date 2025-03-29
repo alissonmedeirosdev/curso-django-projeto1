7a1791d4326518488daf0775fb6dd0e45c4d34d5
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-03-28 23:00:26 -0300

    organizes tests

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,22 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve,reverse
-from . import views
-
-class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 1
-
-    def test_recipe_home_url_is_corret(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_recipe_category_url_is_corret(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipes/category/1/')
-
-    def test_recipe_detail_url_is_corret(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipes/1/')
+from recipes import views
 
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_function_is_correct(self):
