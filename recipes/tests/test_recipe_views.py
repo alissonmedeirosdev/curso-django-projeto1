76a0161abaf38284a227d04b8ed228f324c29a16
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-03-29 15:30:35 -0300

    add new test

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -14,3 +14,18 @@ class RecipeViewsTest(TestCase):
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_return_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home')) 
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_templete(self):
+        response = self.client.get(reverse('recipes:home')) 
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')  
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home')) 
+        self.assertIn(
+            '<h1>Not recipes found here 😓</h1>',
+            response.content.decode('utf-8')
+        ) 
