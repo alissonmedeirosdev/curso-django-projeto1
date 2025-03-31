ec809d573880f489552e75918ddc286c7e1e882e
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-03-31 00:08:44 -0300

    adds test organization method

---
+++ recipes/tests/test_recipe_base.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from recipes.models import Category, Recipe
+from django.contrib.auth.models import User
+
+class RecipeTestBase(TestCase):
+    def setUp(self):
+        category = Category.objects.create(
+            name = 'Category'
+        )
+        
+        author = User.objects.create_user(
+            first_name = 'user',
+            last_name = 'name',
+            username = 'username',
+            password = '123456',
+            email = 'user@gmail.com'
+        )
+        
+        recipe = Recipe.objects.create(
+            category = category,
+            author = author,
+            title = 'Recipe title',
+            description = 'Recipe Description',
+            slug = 'recipe-slug',
+            preparation_time = 10,
+            preparation_time_unit = 'Minutos',
+            servings = 5,
+            servings_unit = 'Porções', 
+            preparation_steps = 'Recipe Preparation Steps',
+            preparation_steps_is_html = False,
+            is_published = True,
+        )
+
+        return super().setUp()
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,10 +1,12 @@
-from django.test import TestCase
 from django.urls import resolve,reverse
 from recipes import views
-from recipes.models import Category, Recipe
-from django.contrib.auth.models import User
+from recipes.models import Recipe
+from .test_recipe_base import RecipeTestBase
 
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeTestBase):
+    
+    def tearDown(self):
+        return super().tearDown()
 
     # TESTS HOME
 
@@ -21,6 +23,7 @@ class RecipeViewsTest(TestCase):
         self.assertTemplateUsed(response, 'recipes/pages/home.html')  
 
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        Recipe.objects.get(pk=1).delete()
         response = self.client.get(reverse('recipes:home')) 
         self.assertIn(
             '<h1>Not recipes found here 😓</h1>',
@@ -28,33 +31,6 @@ class RecipeViewsTest(TestCase):
         ) 
 
     def test_recipe_home_templete_loads_recipes(self):
-        category = Category.objects.create(
-            name = 'Category'
-        )
-        
-        author = User.objects.create_user(
-            first_name = 'user',
-            last_name = 'name',
-            username = 'username',
-            password = '123456',
-            email = 'user@gmail.com'
-        )
-        
-        recipe = Recipe.objects.create(
-            category = category,
-            author = author,
-            title = 'Recipe title',
-            description = 'Recipe Description',
-            slug = 'recipe-slug',
-            preparation_time = 10,
-            preparation_time_unit = 'Minutos',
-            servings = 5,
-            servings_unit = 'Porções', 
-            preparation_steps = 'Recipe Preparation Steps',
-            preparation_steps_is_html = False,
-            is_published = True,
-        )
-        
         response = self.client.get(reverse('recipes:home'))
         response_context_recipes = response.context['recipes']
         content = response.content.decode('utf-8')
