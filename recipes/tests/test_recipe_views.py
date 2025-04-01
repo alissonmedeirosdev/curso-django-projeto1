9cfd3d87d1a0e07319e279bf38bdee9024ac47cd
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-04-01 01:05:54 -0300

    add new tests

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -47,6 +47,14 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1000})) 
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_templete_loads_recipes(self):
+        needed_title = 'This is a category test'
+
+        self.make_recipe(title=needed_title)
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+
+        self.assertIn(needed_title, content)
         
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
@@ -55,3 +63,19 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_detail_view_return_404_if_no_recipes_found(self):
         response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1000})) 
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_templete_loads_the_correct_recipes(self):
+        needed_title = 'This is a detail page - It load one recipe'
+
+        self.make_recipe(title=needed_title)
+        response = self.client.get(
+            reverse(
+                'recipes:recipe', 
+                kwargs={
+                    'id': 1
+                }
+            )
+        )
+        content = response.content.decode('utf-8')
+
+        self.assertIn(needed_title, content)
