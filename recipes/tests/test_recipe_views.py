7714e6666653a18886cb8046b3d29bce1f288f27
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-04-01 02:00:36 -0300

    add new test

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -64,7 +64,15 @@ class RecipeViewsTest(RecipeTestBase):
         content = response.content.decode('utf-8')
 
         self.assertIn(needed_title, content)
-        
+
+    def test_recipe_category_templete_dont_load_recipes_not_published(self):
+        """
+            Test recipe is published False dont show
+        """
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.category.id})) 
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
@@ -88,3 +96,11 @@ class RecipeViewsTest(RecipeTestBase):
         content = response.content.decode('utf-8')
 
         self.assertIn(needed_title, content)
+
+    def test_recipe_detail_templete_dont_load_recipes_not_published(self):
+        """
+            Test recipe is published False dont show
+        """
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.id})) 
+        self.assertEqual(response.status_code, 404)
