6e8e01f0dcb61afa5aaec517d41b19a4b6398286
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-04-02 05:07:05 -0300

    add new tests

--- recipes/tests/test_recipe_models_recipe.py
+++ recipes/tests/test_recipe_models_recipe.py
@@ -1,4 +1,4 @@
-from .test_recipe_base import RecipeTestBase
+from .test_recipe_base import RecipeTestBase, Recipe
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
 
@@ -6,6 +6,25 @@ class RecipeModelTest(RecipeTestBase):
     def setUp(self):
         self.recipe = self.make_recipe()
         return super().setUp()
+    
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            category = self.meke_category(name='Test Default Category'),
+            author = self.make_author(username='newuser'),
+            title = 'Recipe title',
+            description = 'Recipe Description',
+            slug = 'recipe-slug',
+            preparation_time = 10,
+            preparation_time_unit = 'Minutos',
+            servings = 5,
+            servings_unit = 'Porções', 
+            preparation_steps = 'Recipe Preparation Steps',
+        )
+
+        recipe.full_clean()
+        recipe.save()
+
+        return recipe
 
     @parameterized.expand([
             ('title', 65),
@@ -18,3 +37,11 @@ class RecipeModelTest(RecipeTestBase):
         setattr(self.recipe, field, 'A' * (max_length + 1)) # Atribuição de forma dinamica 
         with self.assertRaises(ValidationError):
             self.recipe.full_clean() 
+
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(recipe.preparation_steps_is_html)
+
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(recipe.is_published)
