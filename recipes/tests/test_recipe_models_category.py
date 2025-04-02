bb56e15b45e09f207a596d833d5777864c66a692
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-04-02 10:18:41 -0300

    add test_recipe_category_models_name_max_length_is_65_chars

--- recipes/tests/test_recipe_models_category.py
+++ recipes/tests/test_recipe_models_category.py
@@ -1,4 +1,6 @@
 from .test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
+
 
 class RecipeCategoryModelTest(RecipeTestBase):
     def setUp(self):
@@ -7,10 +9,15 @@ class RecipeCategoryModelTest(RecipeTestBase):
         )
         return super().setUp()
     
-    def test_recipe_category_model_string_representation(self):
+    def test_recipe_category_model_string_representation_is_name_field(self):
         self.assertEqual(
             str(self.category),
             self.category.name
         )
+    
+    def test_recipe_category_models_name_max_length_is_65_chars(self):
+        self.category.name = 'A' * 66
+        with self.assertRaises(ValidationError):
+            self.category.full_clean()
     
    
