25d026f2e6e19088def5a4742ee7c7bfbf52b037
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-04-07 01:07:35 -0300

    remove factory.py in views recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from utils.recipes.factory import make_recipe
 from django.db.models import Q
 from .models import Recipe
 
